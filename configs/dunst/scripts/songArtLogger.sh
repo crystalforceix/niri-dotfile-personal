7847b26473173d9af454b5fd5ba79ce98961b73b
crystalforceix <crystalforceix@gmail.com>
2025-04-17 13:28:47 +0700

    update dunst

--- configs/dunst/scripts/songArtLogger.sh
+++ configs/dunst/scripts/songArtLogger.sh
@@ -2,7 +2,6 @@
 
 TMP_DIR="$HOME/.cache/dunst"
 TMP_COVER_PATH="$TMP_DIR/$DUNST_SUMMARY.png"
-TMP_TEMP_PATH="$TMP_DIR/temp.png"
 
 if [ ! -d "$TMP_DIR" ]; then
 	mkdir -p "$TMP_DIR"
@@ -14,4 +13,3 @@ if [[ $(playerctl -p spotify,%any,firefox,chromium,brave,mpd metadata mpris:artU
 	curl -s "$ART_FROM_SPOTIFY" --output "$TMP_COVER_PATH"
 fi
 
-cp "$TMP_TEMP_PATH" "$TMP_COVER_PATH"
